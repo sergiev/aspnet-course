a660a528945cd07c502a4fa07b8d9aaf8c5f1ac3
sergiev <semyonsever@yandex.ru>
2019-05-15 23:33:09 +0300

    now hidden is seen

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -4,14 +4,21 @@
       <th>Id</th>
       <th>Name</th>
       <th>Address</th>
-      <th></th>
+      <th><a class="button is-primary is-rounded" v-on:click="isHidden = !isHidden" v-if="isHidden">Добавить</a>
+      	<a class="delete is-small" v-if="!isHidden" v-on:click="isHidden = !isHidden"></a></th>
     </thead>
     <tbody>
+    	<tr v-if="!isHidden">
+    		<td> ты</td>
+    		<td> kra</td>
+    		<td> sav</td>
+    		<td> chik</td>
+    	</tr>
       <tr v-for="row in contacts" :key="row.Id">
         <td>{{ row.Id }}</td>
         <td>{{ row.Name }}</td>
         <td>{{ row.Address }}</td>
-        <td><a class="button is-danger" @click="clicked(row)">Удалить</a></td>
+        <td><a class="button is-danger is-rounded" @click="clicked(row)">&nbsp;&nbsp;Удалить&nbsp;&nbsp;</a></td>
       </tr>
     </tbody>
   </table>
@@ -24,7 +31,8 @@ export default {
       	{ Id: 1, Name: "Pavel", Address: "Botanicheskaya" },
         { Id: 2, Name: "ДАННЫЕ", Address: "УДАЛЕНЫ" },
         { Id: 3, Name: "Oleg", Address: "Святая Русь"}
-      ]
+      ],
+      isHidden: true
     }
   }
 };
