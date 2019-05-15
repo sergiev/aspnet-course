6b3c8bbbd68e2c011c0f5ae01a0cfdf89892f4e9
sergiev <semyonsever@yandex.ru>
2019-05-15 21:19:51 +0300

    hello Pavel

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,40 @@
+<template>
+  <table class="table">
+    <thead>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Address</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Pavel</td>
+        <td>Botanicheskaya</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  data: {
+    contacts: [{
+        Id: 1,
+        Name: "Pavel",
+        Address: "Botanicheskaya"
+      },
+      {
+        Id: 2,
+        Name: "Semen",
+        Address: "Botanicheskaya"
+      },
+      {
+        Id: 3,
+        Name: "Oleg",
+        Address: "Dickinstan"
+      }
+    ]
+  }
+};
+</script>
+<style scoped>
+</style>
